c114b566c4a42ceb81eee5f96fd0350d4d9ea6c8
ItsJustRyanBrown <121187970+ItsJustRyanBrown@users.noreply.github.com>
2023-01-09 17:01:05 -0500

    Update accordion.js

--- src/accordion.js
+++ src/accordion.js
@@ -16,6 +16,7 @@ for (i = 0; i < accordionTrg.length; i++) {
     accordionTrg[i].addEventListener("click", function () {
         const accordionItem = this.parentElement;
         accordionItem.classList.toggle('active');
+        accordionItem.querySelector(".accordion-content").classList.add('animated');
         if (this.getAttribute('aria-expanded') === "false"){
             this.setAttribute('aria-expanded', "true");
         }else{
